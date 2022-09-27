9bd5f313ec6c31ea8059512bbcd2e51f9b26e9fa
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-27 13:57:09 -0300

    feat: adiciona o nav em  carrinho

---
+++ front-end/src/pages/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+function Checkout() {
+  return (
+    <main>
+      <NavBar />
+      <p>Estou em Checkout</p>
+    </main>
+  );
+}
+
+export default Checkout;
