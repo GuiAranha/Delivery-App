68cbc2a8e2785d36d03d5c27999308fd2a6bf258
Kaio G <kaiogreco91@gmail.com>
2022-09-27 16:09:44 -0300

    feat: realiza a renderização do nome do usuário na navBar

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../styles/NavBar.module.css';
 
 function NavBar() {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const user = localStorage.getItem('name');
+    if (user) {
+      setName(user);
+    }
+  }, []);
+
+  const userLogout = () => {
+    localStorage.removeItem('name');
+  };
   return (
     <nav className={ styles.mainContainer }>
       <div className={ styles.leftContainer }>
@@ -26,12 +38,13 @@ function NavBar() {
           data-testid="customer_products__element-navbar-user-full-name"
           className={ styles.userName }
         >
-          <div>Nome</div>
+          <div>{ name }</div>
         </div>
         <Link
           to="/"
           data-testid="customer_products__element-navbar-link-logout"
           className={ styles.logout }
+          onClick={ userLogout }
         >
           Sair
         </Link>
