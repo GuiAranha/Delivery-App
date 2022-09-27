1e5d9e1db463eb343e3b89d1bf475d9c92a6b105
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-27 13:56:02 -0300

    feat: cria rotas para pedidos, detalhes de pedidos e carrinho

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,9 @@ import AppProvider from './context/AppProvider';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Products from './pages/Products';
+import Checkout from './pages/Checkout';
+import Order from './pages/Order';
+import OrderDetail from './pages/OrderDetail';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route exact path="/login" element={ <Login /> } />
         <Route exact path="/customer/products" element={ <Products /> } />
+        <Route exact path="/customer/checkout" element={ <Checkout /> } />
+        <Route exact path="/customer/orders/" element={ <Order /> } />
+        <Route exact path="/customer/orders/:id" element={ <OrderDetail /> } />
         <Route exact path="/" element={ <Navigate to="/login" /> } />
         <Route exact path="*" element={ <Navigate to="/login" /> } />
         <Route exact path="/register" element={ <Register /> } />
