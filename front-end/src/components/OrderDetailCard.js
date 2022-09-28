4197eb237c0a31aa6fe55212c98dadb288ca15f5
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-28 10:31:53 -0300

    feat: cria card do item na tela detalhes do pedido

---
+++ front-end/src/components/OrderDetailCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import styles from '../styles/Cards.module.css';
+
+function OrderDetailCard(props) {
+  const { name, price, quantity } = props;
+  const { index } = props;
+  return (
+    <div>
+      <p
+        data-testid={ `customer_order_details__element-order-table-item-number-
+        ${(index + 1)}` }
+      >
+        {(index + 1)}
+      </p>
+      <p
+        data-testid={ `customer_order_details__element-order-table-name-${(index + 1)}` }
+      >
+        {name}
+      </p>
+      <p
+        data-testid={ `customer_order_details__element-order-table-quantity-
+        ${(index + 1)}` }
+      >
+        {quantity}
+      </p>
+      <p
+        data-testid={ `customer_order_details__element-order-table-unit-price-
+        ${(index + 1)}` }
+      >
+        {price}
+      </p>
+      <p
+        data-testid={ `customer_order_details__element-order-table-sub-total-
+        ${(index + 1)}` }
+      >
+        {(price * quantity)}
+      </p>
+    </div>
+  );
+}
+
+OrderDetailCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+};
+
+export default OrderDetailCard;
