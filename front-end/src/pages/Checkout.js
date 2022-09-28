1a690519b03269b9fb1bb66fa36f951a54b43ea2
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-28 09:16:31 -0300

    feat: cria card do item no carrinho e renderiza

--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import CheckoutCard from '../components/CheckoutCard';
 
 function Checkout() {
+  const cardsMock = [
+    {
+      name: 'teste',
+      price: 75,
+      quantity: 2,
+      id: 1,
+    },
+    {
+      name: 'aaaaaa',
+      price: 30,
+      quantity: 2,
+      id: 2,
+    },
+  ];
+
   return (
     <main>
       <NavBar />
       <p>Estou em Checkout</p>
+      <section>
+        {cardsMock.map((elem, index) => (
+          <CheckoutCard key={ index } index={ index } { ...elem } />
+        ))}
+      </section>
     </main>
   );
 }
