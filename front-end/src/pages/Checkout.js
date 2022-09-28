1c45c18edcc03f6b744912021b0fcff2a3bd292d
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-09-28 10:00:31 -0300

    feat: cria os campos da pagina de checkout

--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -17,6 +17,7 @@ function Checkout() {
       id: 2,
     },
   ];
+  const sellersMock = ['andre1', 'andre2'];
 
   return (
     <main>
@@ -26,6 +27,40 @@ function Checkout() {
         {cardsMock.map((elem, index) => (
           <CheckoutCard key={ index } index={ index } { ...elem } />
         ))}
+        <p data-testid="customer_checkout__element-order-total-price">
+          Total: R$
+          {cardsMock.reduce((acc, obj) => acc + (obj.quantity * obj.price), 0)}
+        </p>
+      </section>
+
+      <section>
+        <form>
+          <select data-testid="customer_checkout__select-seller">
+            {sellersMock.map((item, index) => <option key={ index }>{item}</option>)}
+          </select>
+          <label htmlFor="address">
+            Endereço
+            <input
+              data-testid="customer_checkout__input-address"
+              id="addres"
+              type="text"
+            />
+          </label>
+          <label htmlFor="addressNumber">
+            Número
+            <input
+              data-testid="customer_checkout__input-address-number"
+              id="addressNumber"
+              type="text"
+            />
+          </label>
+          <button
+            type="submit"
+            data-testid="customer_checkout__button-submit-order"
+          >
+            Finalizar Pedido
+          </button>
+        </form>
       </section>
     </main>
   );
